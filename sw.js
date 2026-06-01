c1142afd1c21e9c70c163d3dbeb4a635c9e50efe
doofree <wrpbill.fbs@gmail.com>
2026-06-01 07:30:26 +0000

    Update SW zone to 11084846

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11084399
+    "zoneId": 11084846
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
